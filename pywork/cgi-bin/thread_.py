737cc9c217fd4f5ce21226dcda62a8e194fc108c
yanaitamon <rspm068052@gmail.com>
2012-08-02 20:37:15 +0900

    enabled thread_ to read MemoryMappedFile.

--- pywork/cgi-bin/thread_.py
+++ pywork/cgi-bin/thread_.py
@@ -5,18 +5,25 @@ import threading
 import time
 import boostpytest
 
-class test(threading.Thread):
+class timer(threading.Thread):
 	def __init__(self):
 		threading.Thread.__init__(self)
 		self.setDaemon(True)
 		self.i = 0
 
 	def run(self):
+		call = boostpytest.DlgCaller()
+		str = u"start"
+		print dir(call)
 		print "Start."
-		while True:
-			time.sleep(10)
+		while str != "stop":
+			time.sleep(3)
 			self.i += 1
 			print self.i * 10
+			str = call.getCalcStatus()
+			print str
+		else:
+			print "stopped!"
 
 # if __name__ == "__main__":
 # 	t = test()
